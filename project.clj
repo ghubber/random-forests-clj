ea7ef217af814981a226c00ed4eb8252a3993944
Christian Weilbach <christian_weilbach@web.de>
2015-01-28 01:03:31 +0100

    Update deps, fix test.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject random-forests-clj "0.2.0"
   :description "An implementation of Random Forests for classification in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clojure-csv/clojure-csv "2.0.0-alpha1"]
-                 [org.clojure/math.combinatorics "0.0.2"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clojure-csv/clojure-csv "2.0.1"]
+                 [org.clojure/math.combinatorics "0.0.8"]
                  [clj-tokenizer "0.1.0"]
-                 [org.clojure/tools.cli "0.2.1"]]
+                 [org.clojure/tools.cli "0.3.1"]]
    :main random-forests.train)
